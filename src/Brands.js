6903aeb2b0dae55d7d2e6d13ac934651f8ffdfb2
Benjamin Goering <bengoering@gmail.com>
2013-01-05 22:12:26 -0800

    Make deck the right overall width

--- src/Brands.js
+++ src/Brands.js
@@ -1,52 +1,54 @@
 // This will run on the Brands page
 require(['./config'], function () {
 
-require(['fyre', 'streamhub-backbone'], function (fyre, Hub) {
+require(['fyre', 'streamhub-backbone', 'jquery'], function (fyre, Hub, $) {
+    var apps = [];
 	fyre.conv.load({}, [{
 		network: 'livefyre.com',
 		app: 'sdk'
 	}], loadApp);
 	function loadApp (sdk) {
-		var app0 = new Hub({
+		var app0 = apps.push(new Hub({
             sdk: sdk,
             collection: {
                 siteId: "303818",
                 articleId: "51"
             },
             el: document.getElementById("brand-0-env")
-	    });
-	    var app1 = new Hub({
+	    }));
+	    var app1 = apps.push(new Hub({
             sdk: sdk,
             collection: {
 				siteId: "303818",
                 articleId: "2"
             },
             el: document.getElementById("brand-1-env")
-	    });
-	    var app2 = new Hub({
+	    }));
+	    var app2 = apps.push(new Hub({
             sdk: sdk,
             collection: {
                 siteId: "303818",
                 articleId: "26"
             },
             el: document.getElementById("brand-2-env")
-        });
-        var app3 = new Hub({
+        }));
+        var app3 = apps.push(new Hub({
             sdk: sdk,
             collection: {
                 siteId: "303818",
                 articleId: "29"
             },
             el: document.getElementById("brand-3-env")
-        });
-        var app4 = new Hub({
+        }));
+        var app4 = apps.push(new Hub({
             sdk: sdk,
             collection: {
                 siteId: "303818",
                 articleId: "8"
             },
             el: document.getElementById("brand-4-env")
-        });
+        }));
+        $('.deck-columns').css('width', apps.length*307);
 	}
 });
 
