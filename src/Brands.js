5f3b8bf99f4e6aa40c9b245d2eb11116e2ae0690
Benjamin Goering <bengoering@gmail.com>
2013-01-05 22:42:50 -0800

    Add two columns to Brands

--- src/Brands.js
+++ src/Brands.js
@@ -8,6 +8,7 @@ require(['fyre', 'streamhub-backbone', 'jquery'], function (fyre, Hub, $) {
 		app: 'sdk'
 	}], loadApp);
 	function loadApp (sdk) {
+        // Samsung
 		var app0 = apps.push(new Hub({
             sdk: sdk,
             collection: {
@@ -16,22 +17,25 @@ require(['fyre', 'streamhub-backbone', 'jquery'], function (fyre, Hub, $) {
             },
             el: document.getElementById("brand-0-env")
 	    }));
+        // Apple
 	    var app1 = apps.push(new Hub({
             sdk: sdk,
             collection: {
 				siteId: "303818",
-                articleId: "2"
+                articleId: "49"
             },
             el: document.getElementById("brand-1-env")
 	    }));
+        // Microsoft
 	    var app2 = apps.push(new Hub({
             sdk: sdk,
             collection: {
                 siteId: "303818",
-                articleId: "26"
+                articleId: "56"
             },
             el: document.getElementById("brand-2-env")
         }));
+        // Canonical
         var app3 = apps.push(new Hub({
             sdk: sdk,
             collection: {
@@ -40,6 +44,7 @@ require(['fyre', 'streamhub-backbone', 'jquery'], function (fyre, Hub, $) {
             },
             el: document.getElementById("brand-3-env")
         }));
+        // Sony
         var app4 = apps.push(new Hub({
             sdk: sdk,
             collection: {
@@ -47,6 +52,24 @@ require(['fyre', 'streamhub-backbone', 'jquery'], function (fyre, Hub, $) {
                 articleId: "8"
             },
             el: document.getElementById("brand-4-env")
+        }));
+        // Google
+        var app5 = apps.push(new Hub({
+            sdk: sdk,
+            collection: {
+                siteId: "303818",
+                articleId: "60"
+            },
+            el: document.getElementById("brand-5-env")
+        }));
+        // LG
+        var app6 = apps.push(new Hub({
+            sdk: sdk,
+            collection: {
+                siteId: "303818",
+                articleId: "58"
+            },
+            el: document.getElementById("brand-6-env")
         }));
         $('.deck-columns').css('width', apps.length*307);
 	}
