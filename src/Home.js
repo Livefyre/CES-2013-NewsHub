e99a1a18e013fda24ed5c046046951663cfe1ddc
Benjamin Goering <bengoering@gmail.com>
2013-01-08 00:53:53 -0800

    Add tech journalist tweets to home page

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,24 @@
 require(['./config'],
 function () {
 
-//Do something
-require(['jquery'], function ($) {
-	//$('h1').html('Go StreamHub!')
-})
+require(['fyre', 'streamhub-backbone', 'streamhub-isotope',
+	     'jquery', 'mustache', 'text!../src/templates/Instagram.html'],
+function (fyre, Hub, IsotopeView, $, Mustache, InstagramHtml) {
+	fyre.conv.load({}, [{
+		network: 'livefyre.com',
+		app: 'sdk'
+	}], loadHomeApps);
+	function loadHomeApps (sdk) {
+		var wall = new Hub({
+			sdk: sdk,
+	        collection: {
+	            siteId: "303818",
+	            articleId: "42"
+	        },
+	        el: document.getElementById("home-tweets")
+		})
+	}
+});
 
 console.log("Home loaded");
 });
