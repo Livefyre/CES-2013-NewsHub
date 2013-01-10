204715461fe800e3d3a567ecc06c727d37c48d16
Ryan Cheung <ryanc@livefyre.com>
2013-01-10 15:03:11 -0800

    Add TechCrunch

---
+++ src/news/TechCrunch.js
@@ -0,0 +1,38 @@
+// This will run on the index (TechCrunch) page
+require(['../config'],
+function () {
+
+require(['fyre', 'streamhub-backbone', 'streamhub-isotope',
+	     'jquery', 'mustache', '../src/templates/Card', '../src/news/newsCommon'],
+function (fyre, Hub, IsotopeView, $, Mustache, CardTemplate) {
+	fyre.conv.load({
+		network: 'labs.fyre.co'
+	}, [{
+		app: 'sdk'
+	}], loadHomeApps);
+	function loadHomeApps (sdk) {
+		var main = new Hub({
+			sdk: sdk,
+	        collection: {
+	            siteId: "320568",
+	            articleId: "news_techcrunch_chatter"
+	        },
+	        contentViewOptions: {
+	        	template: CardTemplate
+	        },
+	        view: IsotopeView,
+	        el: document.getElementById("techcrunch-main-app")
+		})
+		var tweets = new Hub({
+			sdk: sdk,
+	        collection: {
+	            siteId: "320568",
+	            articleId: "news_techcrunch"
+	        },
+	        el: document.getElementById("techcrunch-tweets")
+		})
+	}
+});
+
+console.log("TechCrunch loaded");
+});
