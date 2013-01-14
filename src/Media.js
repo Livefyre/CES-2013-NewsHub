fda681e78bec7d986c55e2c508bafb50222f84d1
Benjamin Goering <bengoering@gmail.com>
2013-01-14 00:03:47 -0800

    Fix whitespace

--- src/Media.js
+++ src/Media.js
@@ -2,30 +2,30 @@
 require(['./config'], function () {
 
 require(['fyret402', 'streamhub-backbone', 'streamhub-isotope',
-	     'jquery', 'mustache', 'text!../src/templates/Instagram.html'],
+         'jquery', 'mustache', 'text!../src/templates/Instagram.html'],
 function (fyre, Hub, IsotopeView, $, Mustache, InstagramHtml) {
-	fyre.conv.load({}, [{
-		network: 'livefyre.com',
-		app: 'sdk'
-	}], loadMediaWall);
-	function loadMediaWall (sdk) {
-		var wall = new Hub({
-			sdk: sdk,
-	        collection: {
-	            siteId: "303818",
-	            articleId: "69"
-	        },
-	        el: document.getElementById("media-wall-0"),
-	        view: IsotopeView,
-			sources: {
-	            rss: {
-	                template: function (d) {
-	                    return Mustache.compile(InstagramHtml)(d);
-	                }
-	            }
-	        }
-		});
-	}
+    fyre.conv.load({}, [{
+        network: 'livefyre.com',
+        app: 'sdk'
+    }], loadMediaWall);
+    function loadMediaWall (sdk) {
+        var wall = new Hub({
+            sdk: sdk,
+            collection: {
+                siteId: "303818",
+                articleId: "69"
+            },
+            el: document.getElementById("media-wall-0"),
+            view: IsotopeView,
+            sources: {
+                rss: {
+                    template: function (d) {
+                        return Mustache.compile(InstagramHtml)(d);
+                    }
+                }
+            }
+        });
+    }
 });
 
 console.log("Media loaded");
