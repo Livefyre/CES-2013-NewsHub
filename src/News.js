6f3a43041063cd0af63e7eb0c7e02274f73816b4
Benjamin Goering <bengoering@gmail.com>
2013-01-14 22:16:15 -0800

    Use News.js for News page instead of Home.js

--- src/News.js
+++ src/News.js
@@ -1,5 +1,57 @@
-// This will run on the News page
-require(['./config'], function () {
+// This will run on the index (Home) page
+require(['./config'],
+function () {
+
+require(['fyre', 'streamhub-backbone', 'streamhub-isotope',
+	     'jquery', 'mustache', '../src/templates/Card', 'streamhub-slidesjs', 'CES/templates/Hero', '../src/news/newsCommon'],
+function (fyre, Hub, IsotopeView, $, Mustache, CardTemplate, SlideshowView, HeroTemplate, newsCommon) {
+	fyre.conv.load({
+		network: 'labs.fyre.co'
+	}, [{
+		app: 'sdk'
+	}], loadHomeApps);
+	function loadHomeApps (sdk) {
+		var hero = new Hub({
+			sdk: sdk,
+			collection: {
+				siteId: "320568",
+				articleId: "home_editorial"
+			},
+			el: document.getElementById('hero'),
+			view: SlideshowView,
+			contentViewOptions: {
+				template: HeroTemplate
+			}
+		});
+		var main = new Hub({
+			sdk: sdk,
+	        collection: {
+	            siteId: "320568",
+	            articleId: "home_news"
+	        },
+	        contentViewOptions: {
+	        	template: CardTemplate
+	        },
+	        view: IsotopeView,
+	        viewOptions: {
+	        	isotope: {
+	        		masonry: {
+	        			columnWidth: 213
+	        		}
+	        	}
+	        },
+	        el: document.getElementById("home-main-app")
+		})
+		var tweets = new Hub({
+			sdk: sdk,
+	        collection: {
+	            siteId: "320568",
+	            articleId: "home_fromthefloor"
+	        },
+	        el: document.getElementById("home-tweets")
+		})
+	}
+});
 
 console.log("News loaded");
 });
